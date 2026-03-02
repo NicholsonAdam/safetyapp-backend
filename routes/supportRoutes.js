6184f2027013b3bd1ee4b43bf525676f611a05da
Adam NICHOLSON <adam.nicholson1@daltile.com>
2026-03-02 11:07:19 -0600

    pic

--- routes/supportRoutes.js
+++ routes/supportRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
 const { createSupport } = require("../controllers/supportController");
 
 const router = express.Router();
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "..", "uploads"));
+  },
+  filename: (req, file, cb) => {
+    const unique = Date.now() + "-" + file.originalname;
+    cb(null, unique);
+  }
+});
+
+const upload = multer({ storage });
 
 router.post("/create", upload.single("photo"), createSupport);
 
