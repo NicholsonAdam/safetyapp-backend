e58d9f908cae95cbe330a6d83e8eae21b64efa0e
Adam NICHOLSON <adam.nicholson1@daltile.com>
2026-04-27 11:34:10 -0500

    REDEPLOY VERIFIER

--- routes/documents.js
+++ routes/documents.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+const router = require("express").Router(); //Test to force a redeploy
 const controller = require("../controllers/documentsController");
 
 router.get("/", controller.getDocumentsByFolder);
