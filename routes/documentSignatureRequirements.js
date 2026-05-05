0fdab342455886b1588426dd6751897598f3a3ac
Adam NICHOLSON <adam.nicholson1@daltile.com>
2026-05-05 09:46:22 -0500

    signature 5

--- routes/documentSignatureRequirements.js
+++ routes/documentSignatureRequirements.js
@@ -3,27 +3,27 @@
 const express = require('express');
 const router = express.Router();
 
-const documentSignatureRequirementController = require('../controllers/documentSignatureRequirementController');
+const documentSignatureRequirementController = require('../controllers/documentSignatureRequirementsController');
 
 // Assign signature requirements
-router.post('/', documentSignatureRequirementController.assignRequirements);
+router.post('/', documentSignatureRequirementsController.assignRequirements);
 
 // Get requirements for a document version
 router.get(
   '/document/:documentId/version/:versionId',
-  documentSignatureRequirementController.getRequirementsForDocumentVersion
+  documentSignatureRequirementsController.getRequirementsForDocumentVersion
 );
 
 // Get requirements for a specific employee
 router.get(
   '/employee/:employeeId',
-  documentSignatureRequirementController.getRequirementsForEmployee
+  documentSignatureRequirementsController.getRequirementsForEmployee
 );
 
 // Leader dashboard for a document version
 router.get(
   '/dashboard/:documentId/:versionId',
-  documentSignatureRequirementController.getDashboardForDocumentVersion
+  documentSignatureRequirementsController.getDashboardForDocumentVersion
 );
 
 module.exports = router;
